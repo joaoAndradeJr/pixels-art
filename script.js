c208ed8e50db508a133a4caa7b63efab9256110f
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-18 19:30:41 -0400

    add: color palette

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// scripts aqui
+const colorsArray = ['black', 'green', 'yellow', 'red'];
+const pallete = document.getElementById('color-palette');
+const pixels = document.getElementById('pixel-board');
+
+for (let i = 0; i < 4; i += 1) {
+  const p = document.createElement('p');
+  p.setAttribute('class', 'color');
+  p.style.backgroundColor = colorsArray[i];
+  pallete.appendChild(p);
+}
+
+for (let i = 0; i < 5; i += 1) {
+  const p = document.createElement('p');
+  p.setAttribute('class', 'pixel');
+  pixels.appendChild(p);
+}
